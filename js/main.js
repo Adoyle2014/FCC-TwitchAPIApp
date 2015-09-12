2690c39c31784344f2a9d2d790491f013aa011a3
Adam Doyle <dominofoe@gmail.com>
2015-09-12 15:08:13 -0400

    refactored code

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function main() {
 
     twitchSearch();
 
-    $("#page-previous").on('click', function() {
+    /*$("#page-previous").on('click', function() {
        if(prev) {
            pagePrev();
        }
@@ -27,16 +27,15 @@ function main() {
            pageNext();
        }
 
-    });
+    });*/
 
 
     function twitchSearch() {
-        var searchTerm = "";
-        var searchType = "";
-        var params = {};
-        var url = "";
 
-        $(".search-type").on('click', function (e) {
+        var searchType = "channels";
+
+
+        $(".search-type").on('click', function(e) {
             e.preventDefault();
             searchType = $(this).attr("data-id");
 
@@ -44,23 +43,30 @@ function main() {
 
         $("#search-btn").on('click', function (e) {
             e.preventDefault();
-            if(!searchType) {
-                searchType = "channels";
-            } else if (searchType === "games") {
-                url = "https://api.twitch.tv/kraken/search/games?type=suggest"
-            } else {
-                url = "https://api.twitch.tv/kraken/search/" + searchType + "?";
-            }
             searchTerm = $("#search-input").val();
-            params = {'q': searchTerm, 'limit': 13};
-            var call = new twitchApiCall(searchType, searchTerm, params, url);
-            call.call();
+            var params = {'q': searchTerm, 'limit': 12};
+            var url = "https://api.twitch.tv/kraken/search/" + searchType + "?";
+
+            if(searchType === "games") {
+                url = "https://api.twitch.tv/kraken/search/games?type=suggest";
+                var gamesSearch = new TwitchApiCall(searchType, searchTerm, params, url);
+                gamesSearch.call();
+            } else if (!searchType) {
+
+                var defaultSearch = new TwitchApiCall(searchType, searchTerm, params, url);
+                defaultSearch.call();
+            } else {
+                var chanStreSearch = new TwitchApiCall(searchType, searchTerm, params, url);
+                chanStreSearch.call();
+            }
         });
 
 
     }
 
-    function twitchApiCall(searchType, searchTerm, params, url) {
+
+
+    function TwitchApiCall(searchType, searchTerm, params, url) {
         this.searchType = searchType;
         this.searchTerm = searchTerm;
         this.params = params;
@@ -77,7 +83,7 @@ function main() {
                     var data = [];
                     next = response._links.next;
                     prev = response._links.prev;
-                    console.log(next + " - " + self);
+
                     if (searchType === "channels") {
                         data = response.channels;
                     } else if (searchType === "streams") {
@@ -93,6 +99,42 @@ function main() {
             $("#page-title").html("Twitch.tv/" + searchType + "/" + searchTerm);
             $("#sort-pop").addClass('active');
         }
+    }
+
+
+    function displaySearchResults(data) {
+        console.log(data);
+
+        $(".index").fadeOut('slow');
+        $("#main-row").fadeOut('slow', function () {
+            $("#main-row").empty();
+            for (var i = 0; i < data.length; i++) {
+                var displayName = data[i].display_name;
+                var logo = '';
+                if(data[i].logo) {
+                    logo = data[i].logo;
+                } else {
+                    logo = "images/offline.png"
+                }
+                var views = data[i].views;
+                var followers = data[i].followers;
+                var twitchLink = data[i].url;
+
+                $("#main-row").append('<div class="col-md-4"><div class="well stream-blocks"><div class="row"><div class="col-sm-12 text-center"><h4>' + displayName + '</h4></div></div><div class="row"><div class="col-md-6"><img class="stream-images img-responsive" src="' + logo + '"></div><div class="col-md-6 text-center"><h5>offline</h5><h5>' + views + ' Total Views</h5><h5>' + followers + ' Followers</h5><h5><a href="' + twitchLink + '" target="_blank">View on Twitch</a> </h5></div></div></div></div>');
+
+            }
+        });
+
+        $("#main-row").fadeIn('slow');
+
+    }
+
+    function pageNext() {
+        console.log(next);
+    }
+
+    function pagePrev() {
+        console.log(prev);
     }
 
 
@@ -141,28 +183,7 @@ function main() {
     }*/
 
 
-    function displaySearchResults(data) {
-        console.log(data);
-        $(".index").fadeOut('slow');
-        $("#main-row").fadeOut('slow', function () {
-            $("#main-row").empty();
-            for (var i = 0; i < data.length; i++) {
-                $("#main-row").append('<div class="col-md-4"><div class="well stream-blocks"><div class="row"><div class="col-sm-12 text-center"><h4>' + data[i].display_name + '</h4></div></div><div class="row"><div class="col-md-6"><img class="stream-images img-responsive" src="' + data[i].logo + '"></div><div class="col-md-6 text-center"><h5>offline</h5><h5>' + data[i].views + ' Total Views</h5><h5>' + data[i].followers + ' Followers</h5><h5><a href="' + data[i].url + '" target="_blank">View on Twitch</a> </h5></div></div></div></div>');
 
-            }
-        });
-
-        $("#main-row").fadeIn('slow');
-
-    }
-
-    function pageNext() {
-        console.log(next);
-    }
-
-    function pagePrev() {
-        console.log(prev);
-    }
 
 }
 
